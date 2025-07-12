a58bace1366a8646dacae04244d886e71a7254f1
maxweber36 <woshiweiben1@gmail.com>
2025-07-12 22:33:09 +0800

    style: format webpack config and remove empty line in api.js

--- next.config.js
+++ next.config.js
@@ -12,7 +12,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  webpack: config => {
+  webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
