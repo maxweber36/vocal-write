a4120a239e7cd83949c19e3537a3187291bf36e5
maxweber36 <woshiweiben1@gmail.com>
2025-07-12 11:26:53 +0800

    feat: replace svg copy icon with png image
    Use next/image for better performance and maintainability

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import { useEffect, useState, useCallback } from 'react'
+import Image from 'next/image'
+import copyIcon from '../src/assets/icon-copy.png'
 
 import { useAsr, MAX_RECORDING_DURATION } from '../hooks/useAsr'
 import Button from '../src/components/ui/Button'
@@ -156,27 +158,16 @@ export default function Home() {
               polishedTranscript={polishedTranscript}
               onPolishedTranscriptChange={handlePolishedTranscriptChange}
             />
-            {!isRecording && !isPolishing && polishedTranscript && (
-              <button
-                onClick={handleCopy}
-                className="absolute bottom-2 right-2 p-2 rounded-md hover:bg-gray-100 focus:outline-none"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
+            <div>
+              {!isRecording && !isPolishing && polishedTranscript && (
+                <button
+                  onClick={handleCopy}
+                  className="absolute bottom-2 right-2 p-2 rounded-md hover:bg-gray-100 focus:outline-none"
                 >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth={2}
-                    d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
-                  />
-                </svg>
-              </button>
-            )}
+                  <Image src={copyIcon} alt="Copy icon" width={20} height={20} />
+                </button>
+              )}
+            </div>
           </div>
 
           {showCopySuccess && (
