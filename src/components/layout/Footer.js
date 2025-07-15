f535789f9161692c5db56f8254230d58f2c544c2
maxweber36 <woshiweiben1@gmail.com>
2025-07-15 14:59:10 +0800

    build(electron): add dmg and zip targets for mac in M chip and Intel chip

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,15 @@ const Footer = () => {
   return (
     <footer className="w-full text-center py-4 text-gray-400 text-xs">
       {/* text-gray-500 用于设置字体颜色为灰色，text-sm 用于设置字体大小为小号 */}
-      created by <a href="https://maxweber36.github.io/archives/" className="text-blue-500 hover:text-blue-700 hover:underline" target="_blank" rel="noopener noreferrer">阿犇Weber</a>
+      created by{' '}
+      <a
+        href="https://maxweber36.github.io/archives/"
+        className="text-blue-500 hover:text-blue-700 hover:underline"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        阿犇Weber
+      </a>
     </footer>
   )
 }
